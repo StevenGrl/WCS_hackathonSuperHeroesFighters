7ee0038e443b5206d4b8c8c172ac9302be967186
StevenGrl <steven.grillon.45@gmail.com>
2018-04-05 21:50:23 +0200

    create directory for images

--- src/Model/Attack.php
+++ src/Model/Attack.php
@@ -16,72 +16,84 @@ class Attack
             'name' => 'Jet de livre',
             'stat' => ['INT', 40],
             'form' => '',
+            'icon' => '',
             'image' => 'https://media.giphy.com/media/l0HlQI7WEN5gmUzgQ/giphy.gif',
             'power' => 0
         ],[
             'name' => 'Ruse',
             'stat' => ['INT', 70],
             'form' => '',
+            'icon' => '',
             'image' => 'https://media.giphy.com/media/MVlRUmPRsAnRe/giphy.gif',
             'power' => 0
         ],[
             'name' => 'Sortilège',
             'stat' => ['INT', 100],
             'form' => '',
+            'icon' => '',
             'image' => 'https://media.giphy.com/media/l0ExsgrTuACbtPaqQ/giphy.gif',
             'power' => 0
         ],[
             'name' => 'Petit projectile',
             'stat' => ['STR', 40],
             'form' => '',
+            'icon' => '',
             'image' => 'https://media.giphy.com/media/XwhEhu60cZI88/giphy.gif',
             'power' => 0
         ],[
             'name' => 'Gros projectile',
             'stat' => ['STR', 70],
             'form' => '',
-            'image' => '',
+            'icon' => '',
+            'image' => 'asset/images/gifAttack/grosProjectile.gif',
             'power' => 0
         ],[
             'name' => 'Gros rocher',
             'stat' => ['STR', 100],
             'form' => '',
+            'icon' => '',
             'image' => '',
             'power' => 0
         ],[
             'name' => 'Sphère lumineuse',
             'stat' => ['POW', 40],
             'form' => '',
+            'icon' => '',
             'image' => '',
             'power' => 0
         ],[
             'name' => 'Boule d\'énergie',
             'stat' => ['POW', 70],
             'form' => '',
+            'icon' => '',
             'image' => '',
             'power' => 0
         ],[
             'name' => 'Rayon laser',
             'stat' => ['POW', 100],
             'form' => '',
+            'icon' => '',
             'image' => '',
             'power' => 0
         ],[
             'name' => 'Claque',
             'stat' => ['CMB', 40],
             'form' => '',
-            'image' => '',
+            'icon' => '',
+            'image' => 'asset/images/gifAttack/gifle.gif',
             'power' => 0
         ],[
-            'name' => 'Coup de poing',
+            'name' => 'Kicks',
             'stat' => ['CMB', 70],
             'form' => '',
+            'icon' => '',
             'image' => '',
             'power' => 0
         ],[
-            'name' => 'German suplex',
+            'name' => 'MEGA PUNCH',
             'stat' => ['CMB', 100],
             'form' => '',
+            'icon' => 'asset/images/iconAttack/attack.png',
             'image' => '',
             'power' => 0
         ],
