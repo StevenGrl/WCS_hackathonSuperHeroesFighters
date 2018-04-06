7ecbf1107299e7bdad4434c1881c34735b2637ef
Brice Jaugin <syara.legnome@gmail.com>
2018-04-06 10:04:50 +0200

    js select page

--- public/assets/js/selectplayer.js
+++ public/assets/js/selectplayer.js
@@ -1,14 +1,34 @@
 $(document).ready(function($){
     $('.thumbnail').click(function(){
         if ($('#player1').val()==''){
-
-            $('#player1-image').attr('src',$(this).children().attr('src').replace('sm','md'));
-            $('#name1').html($(this).attr('data'));
+            image = $(this);
             $('#player1-image').attr('data',$(this).attr('id'));
+            $('#player1-image').fadeOut( "slow", function() {
+                // Animation complete
+                $('#player1-image').attr('src',image.children().attr('src').replace('sm','md'));
+                $('#player1-image').fadeIn( "slow", function() {});
+            });
+            $('#name1').fadeOut( "slow", function() {
+                // Animation complete
+                $('#name1').html(image.attr('data'));
+                $('#name1').fadeIn( "slow", function() {});
+            });
+
+
+
         } else if ($('#player2').val()=='') {
-            $('#player2-image').attr('src',$(this).children().attr('src').replace('sm','md'));
-            $('#name2').html($(this).attr('data'));
+            image = $(this);
             $('#player2-image').attr('data',$(this).attr('id'));
+            $('#player2-image').fadeOut( "slow", function() {
+                // Animation complete
+                $('#player2-image').attr('src',image.children().attr('src').replace('sm','md'));
+                $('#player2-image').fadeIn( "slow", function() {});
+            });
+            $('#name2').fadeOut( "slow", function() {
+                // Animation complete
+                $('#name2').html(image.attr('data'));
+                $('#name2').fadeIn( "slow", function() {});
+            });
         }
     });
 
